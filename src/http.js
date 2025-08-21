37c674a61ec2dc4a6697b53c9545ac17bc3dca8f
ShanQing <31645014+vcertion@users.noreply.github.com>
2025-08-21 21:26:08 +0800

    Update http.js

--- src/http.js
+++ src/http.js
@@ -1,7 +1,17 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 
-axios.defaults.timeout = 5000;
-axios.defaults.baseURL = 'http://127.0.0.1:5000';
+// axios.defaults.timeout = 5000;
+// axios.defaults.baseURL = 'http://127.0.0.1:5000';
 
-export default axios;
+// export default axios;
+
+import axios from 'axios'
+
+const http = axios.create({
+  baseURL: 'https://SQ246-gis-web.hf.space',
+  timeout: 15000
+})
+
+export default http
+
